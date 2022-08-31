1ccf7e147b9eac736f8b57b5774e56ce1214b9a2
Gabriel <62681139+gabrielfeliphe@users.noreply.github.com>
2022-08-31 19:58:01 -0300

    chore: db

--- d.js
+++ d.js
@@ -1,5 +1,8 @@
-const CyclicDB = require('cyclic-dynamodb')
-const db = CyclicDB() 
+const db = require('cyclic-dynamodb')
+
+
+process.env.CYCLIC_DB = process.env.CYCLIC_DB || 'db-sdkCyclicDB'
+process.env.AWS_REGION = process.env.AWS_REGION || 'us-east-1'
 
 baseItem = async function (){
     
