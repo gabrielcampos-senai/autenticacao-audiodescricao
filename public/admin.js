a0ad271d1b43b4acdd8bdc4bb55c50a22db6f26e
Gabriel Campos <gabriel.campos@fulltechpro.com.br>
2022-09-23 16:12:53 -0300

    testes

--- public/admin.js
+++ public/admin.js
@@ -1,22 +1,11 @@
-(function($) {
-    $.fn.extend({
-      populateOpts: function(options) {
-        var defaults = {
-          source: [],
-        };
-  
-        var options = $.extend(defaults, options);
-  
-        return this.each(function(index, row) {
-          if (options.source && options.source.length > 0) {
-            for (var i = 1; i <= options.source[index]; i++) {
-              $(this).append($("<option>").text(i).val(i));
-            }
-          }
-        });
-      }
-    });
-  })(jQuery);
-  
-  // Now you can do the following
-  $('#email').populateOpts({ source: [5, 2, 2, 3, 5] });
+var express = require('express');
+var router = express.Router();
+var otherRouter = require('./users')
+
+/* GET groups listing. */
+router.get('', function(req, res, next) {
+
+    // call the get on users and retrieve all data from that request
+
+    res.send('GET for the groups');
+});
